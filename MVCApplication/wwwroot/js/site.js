cfc98d968d38dbab143d9a39626a26ee45a263b1
Allison Birkrem <allisonbirkrem@gmail.com>
2024-03-10 21:26:40 -0500

    Start with component libraries

--- MVCApplication/wwwroot/js/site.js
+++ MVCApplication/wwwroot/js/site.js
@@ -2,3 +2,6 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+import '@material/web/button/filled-button.js';
+import '@material/web/button/outlined-button.js';
+import '@material/web/checkbox/checkbox.js';
